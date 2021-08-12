03614556c061ca6387af7378f4e44cb5ddf59b8c
Natalie Orsi <thisis@natalies.email>
2021-08-12 16:26:41 -0700

    smallest() and very basic rendering

--- tree.js
+++ tree.js
@@ -22,14 +22,14 @@ class Tree {
             // Add to left if there is room, else traverse left branch
             if (this.root.tryAddLeft(num)) {
                 return;
-            } else return this.searchTree(this.root.left);
+            } else return this.addLeaf(this.root.left);
         
         // Larger num to the right, recursively
         } else if (num > this.root.num) {
             // Add to right if there is room, else traverse right branch
             if (this.root.tryAddRight(num)) {
                 return;
-            } else return this.searchTree(this.root.right);
+            } else return this.addLeaf(this.root.right);
 
         // Disregard duplicate adds        
         } else {
@@ -50,14 +50,32 @@ class Tree {
             // 2 children: set root equal to smallest on right branch
             root = root.right.smallest();
             root.right = this.removeLeaf(root.right, root.num);
-
-            // This will probably need rebalancing
+        
+        // Recursively call removeLeaf on children until num === root.num
         } else if (num < root.num) {
             root.left = this.removeLeaf(root.left, num);
         } else {
             root.right = this.removeLeaf(root.right, num);
         }
     }
+
+    render() {
+        // Don't bother if there's nothing there
+        if (this.emptyTree()) return;
+
+        // Build tree structure before appending to body
+        const body = document.getElementsByTagName("body")[0];
+        const tree = document.createElement("div");
+        tree.setAttribute("id", "tree");
+        
+        // Create root
+        tree.appendChild(this.root.display());
+        // appendChild() for right branches, insertBefore() for left branches
+
+        // Append tree to body
+        body.appendChild(tree);
+    }
+
 
 
 }
@@ -85,7 +103,45 @@ class Leaf {
         return true;
     }
 
-    smallest(num) {
-        return;
+    smallest() {
+        if (this.left === null) return this.num;
+
+        // Recursively calling smallest() on left child if it exists
+        return this.left.smallest();
+    }
+
+    display() {
+        const d = document.createElement("div");
+        d.setAttribute("id", this.num + "");
+        d.innerHTML = this.num + " ";
+        return d;
     }
 }
+
+function randomInt(max) {
+    return ~~(Math.random()*max);
+}
+function randomTree() {
+    const rt = new Tree();
+    randomLeaves = [...Array(randomInt(15))].map(leaf=>randomInt(15));
+    randomLeaves.forEach(leaf => {
+        if (leaf !== 0) {
+            rt.addLeaf(leaf)
+        }
+        else {
+            // Remove 0s
+            index = randomLeaves.indexOf(leaf);
+            randomLeaves.splice(index, 1);
+        }
+    });
+
+    // Put randomLeaves in order for display as h1
+    randomLeaves.sort(function(a, b) { return a - b; });
+    const h = document.createElement("h1");
+    h.innerHTML = randomLeaves + "";
+    document.getElementsByTagName("body")[0].append(h);
+
+    return rt;
+}
+
+randomTree().render();
