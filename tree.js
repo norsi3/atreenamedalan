a9b6a308b8626f31c61ccba6badfad5edfd7cf98
Natalie Orsi <thisis@natalies.email>
2021-08-12 15:19:04 -0700

    removeLeaf
    Got rid of parent = this.root to be more consistent across functions. Added removeLeaf.

--- tree.js
+++ tree.js
@@ -16,25 +16,46 @@ class Tree {
         if (this.emptyTree()) return this.plantTree(num);
         
         // Traverse the tree to recursively sort num relative to parent(s)
-        const parent = this.root;
 
         // Smaller num to the left, recursively
-        if (num < parent) {
+        if (num < this.root.num) {
             // Add to left if there is room, else traverse left branch
-            if (parent.tryAddLeft(num)) {
+            if (this.root.tryAddLeft(num)) {
                 return;
-            } else return this.searchTree(parent.left);
+            } else return this.searchTree(this.root.left);
         
         // Larger num to the right, recursively
-        } else if (num > parent) {
+        } else if (num > this.root.num) {
             // Add to right if there is room, else traverse right branch
-            if (parent.tryAddRight(num)) {
+            if (this.root.tryAddRight(num)) {
                 return;
-            } else return this.searchTree(parent.right);
+            } else return this.searchTree(this.root.right);
 
         // Disregard duplicate adds        
         } else {
             return null;
+        }
+    }
+
+    removeLeaf(num, root=this.root) {
+        // Empty root? Return null
+        if (typeof(root) !== Leaf) return null;
+
+        // Removing the root node?
+        if (num === root.num) {
+            // 0-1 children: null or use only existing branch
+            if (root.left === null) return root.right;
+            if (root.right === null) return root.left;
+
+            // 2 children: set root equal to smallest on right branch
+            root = root.right.smallest();
+            root.right = this.removeLeaf(root.right, root.num);
+
+            // This will probably need rebalancing
+        } else if (num < root.num) {
+            root.left = this.removeLeaf(root.left, num);
+        } else {
+            root.right = this.removeLeaf(root.right, num);
         }
     }
 
@@ -51,16 +72,20 @@ class Leaf {
     }
 
     tryAddLeft(num) {
-        if (this.left != null) return false;
+        if (this.left !== null) return false;
 
         this.left = new Leaf(num);
         return true;
     }
 
     tryAddRight(num) {
-        if (this.right != null) return false;
+        if (this.right !== null) return false;
 
         this.right = new Leaf(num);
         return true;
+    }
+
+    smallest(num) {
+        return;
     }
 }
