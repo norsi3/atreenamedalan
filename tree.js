67be259e1d6e108f6a5a597eb161ed409ff6aee9
Natalie Orsi <thisis@natalies.email>
2021-08-13 08:50:39 -0700

    BST logic, toString
    Basic BST logic complete, added toString methods

--- tree.js
+++ tree.js
@@ -7,8 +7,30 @@ class Tree {
         return this.root === null;
     }
     
-    insertNum(num) {
-        this.root = this.addLeaf(num, this.root);
+    addLeaf(num) {
+        if (this.emptyTree()) { 
+            this.plantTree(num);
+            return this.root;
+        }
+
+        const insertNum = (num, root) => {
+            // Traverse the tree to recursively sort num relative to parent(s)
+            // Smaller num to the left, recursively
+            if (num < root.num) {
+                // Add to left if there is room, else traverse left branch
+                if (!root.tryAddLeft(num)) insertNum(num, root.left);
+            
+            // Larger num to the right, recursively
+            } else if (num > root.num) {
+                // Add to right if there is room, else traverse right branch
+                if (!root.tryAddRight(num)) insertNum(num, root.right);
+            }
+        };
+
+        insertNum(num, this.root);
+
+        return this.root;
+        
     }
 
     plantTree(num) {
@@ -16,55 +38,37 @@ class Tree {
         return this.root;
     }
 
+    removeLeaf(num) {
+        const removeNum = (num, root, rootSetter) => {
+            // Removing the root node?
+            if (num === root.num) {
+                // 0-1 children: null or use only existing branch
+                if (root.left === null) {
+                    rootSetter(root.right);
+                } else if (root.right === null) {
+                    rootSetter(root.left);
+                } else {
+                    // 2 children: set root equal to smallest on right branch
+                    const left = root.left;
+                    const right = root.right;
+                    const small = root.right.smallest();
+                    small.left = left;
+                    rootSetter(right);
+    
+                }
 
-    addLeaf(num, root) {
-        // Start a new tree if this is the first leaf
-        if (this.emptyTree()) { 
-            this.plantTree(num);
-            return this.root;
-        }
-        
-        // Traverse the tree to recursively sort num relative to parent(s)
-        // Smaller num to the left, recursively
-        if (num < root.num) {
-            // Add to left if there is room, else traverse left branch
-            if (this.root.tryAddLeft(num)) {
-                return null;
-            } else return this.addLeaf(num, root.left);
-        
-        // Larger num to the right, recursively
-        } else if (num > root.num) {
-            // Add to right if there is room, else traverse right branch
-            if (root.tryAddRight(num)) {
-                return null;
-            } else return this.addLeaf(num, root.right);
+                this.destroyLeafByNum(num);
+    
+            // Recursively call removeLeaf on children until num === root.num
+            } else if (num < root.num) {
+                removeNum(num, root.left, (x) => root.left = x);
+            } else {
+                removeNum(num, root.right, (x) => root.right = x); 
+            }
+    
+        };
 
-        // Disregard duplicate adds        
-        } else {
-            return null;
-        }
-    }
-
-    removeLeaf(num, root=this.root) {
-        // Empty root? Return null
-        if (typeof(root) !== Leaf) return null;
-
-        // Removing the root node?
-        if (num === root.num) {
-            // 0-1 children: null or use only existing branch
-            if (root.left === null) return root.right;
-            if (root.right === null) return root.left;
-
-            // 2 children: set root equal to smallest on right branch
-            root = root.right.smallest();
-            root.right = this.removeLeaf(root.right, root.num);
-        
-        // Recursively call removeLeaf on children until num === root.num
-        } else if (num < root.num) {
-            root.left = this.removeLeaf(root.left, num);
-        } else {
-            root.right = this.removeLeaf(root.right, num);
-        }
+        removeNum(num, this.root, (x) => this.root = x);
     }
 
     render() {
@@ -83,6 +87,16 @@ class Tree {
 
         // Append tree to body
         body.appendChild(tree);
+    }
+
+    destroyLeafByNum(num) {
+        if (this.emptyTree()) return;
+
+        // TODO
+    }
+
+    toString() {
+        return String(this.root);
     }
 
 
@@ -111,7 +125,7 @@ class Leaf {
     }
 
     smallest() {
-        if (this.left === null) return this.num;
+        if (this.left === null) return this;
 
         // Recursively calling smallest() on left child if it exists
         return this.left.smallest();
@@ -123,11 +137,19 @@ class Leaf {
         d.innerHTML = this.num + " ";
         return d;
     }
+
+    toString() {
+        const root = this.num;
+        const left = ("\n" + this.left).replaceAll("\n","\n\t");
+        const right = ("\n" + this.right).replaceAll("\n","\n\t");
+        return root + left + right;
+    }
 }
 
 function randomInt(max) {
     return ~~(Math.random()*max);
 }
+
 function randomTree() {
     const rt = new Tree();
     randomLeaves = [...Array(randomInt(15))].map(leaf=>randomInt(15));
@@ -136,7 +158,7 @@ function randomTree() {
     randomLeaves = [...new Set(randomLeaves)];
     randomLeaves.forEach(leaf => {
         if (leaf !== 0) {
-            rt.insertNum(leaf)
+            rt.addLeaf(leaf)
         }
     });
 
@@ -149,4 +171,28 @@ function randomTree() {
     return rt;
 }
 
-randomTree().render();
+// randomTree().render();
+
+function fixedTree() {
+    const ft = new Tree();
+    const leaves = [1,2,3,4,5,6];
+    leaves.forEach(
+        leaf => {
+            ft.addLeaf(leaf);
+        }
+    );
+
+    const h = document.createElement("h1");
+    h.innerHTML = leaves + "";
+    document.getElementsByTagName("body")[0].append(h);
+    return ft;
+}
+
+const t = randomTree();
+t.addLeaf(4);
+t.addLeaf(3.9);
+
+t.render();
+console.log(String(t));
+t.removeLeaf(4);
+console.log(String(t));
